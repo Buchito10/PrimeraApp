38c8a994b03567f07f776b47c7b33a225310b9a3
Buchito10 <hugo.rdgz0705@gmail.com>
2024-10-11 10:34:29 -0600

    Primer commit

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import "@/app/not-found.css"
+export default function NotFound(){
+    return(
+        <div className="centrar">
+
+        <h1>Error 404</h1>
+        <p>Página no encontrada</p>
+
+        </div>
+    );
+}
